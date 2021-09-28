e8ad8eb2c08ea3b20777025ff6b4cd43da908747
Kocharigka <onolog@yandex.ru>
2021-09-28 22:05:57 +0500

    add skeletron logic

--- Assets/Scripts/EnemyArcher.cs
+++ Assets/Scripts/EnemyArcher.cs
@@ -4,11 +4,63 @@ using UnityEngine;
 
 public class EnemyArcher : Enemy
 {
+    private Shooter shooter;
+    SectorChooser chooser = new SectorChooser();
+    private float cooldown = 3f;
+    private float cdTimer;
+    private float dangerRadius = 10f;
     void Awake()
     {
         EnemyName = "Sceleton-archer";
         MaxHealth = 20;
         HealhBarOffset = new Vector3(0, 2f, 0);
-        MoveSpeed = 2;
+        DefaultMoveSpeed = 3;
+        shooter = GetComponent<Shooter>();
+        shooter.Cooldown = 2;
+        shooter.ProjectileName = "enemyArrow";
+        shooter.Delay = 0;
+    }
+
+    public override void FollowPlayer(Vector2 playerPosition)
+    {
+        Debug.Log(MoveSpeed);
+        if (!IsStunned && active && Vector2.Distance(transform.position, playerPosition) <= dangerRadius)
+        {
+            Animator.SetBool("CanMove", true);
+        }
+        Vector2 playerDir = chooser.sectorToVector(transform.position, Player.transform.position);
+        Animator.SetFloat("Horizontal", playerDir.x);
+        Animator.SetFloat("Vertical", playerDir.y);
+        if (Vector2.Distance(transform.position, playerPosition) <= dangerRadius)
+        {            
+                Vector2 run = (Vector2)transform.position - playerPosition;
+                transform.position = Vector2.MoveTowards(transform.position, (Vector2)transform.position + run, MoveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Animator.SetBool("CanMove", false);
+        }
+    }
+    public override void PerformAttack(Vector2 playerPosition)
+    {
+        if (cdTimer <= 0)
+        {
+            Animator.SetTrigger("Attack");
+            cdTimer = cooldown;
+        }
+        else
+        {
+            cdTimer -= Time.deltaTime;
+
+        }
+    }
+    public override void SetDefault()
+    {
+        MoveSpeed = DefaultMoveSpeed;
+    }
+
+    public void ShootProjectile()
+    {
+        shooter.Shoot(transform.position, chooser.getAngle(getPlayerPosition(), transform.position));
     }
 }
