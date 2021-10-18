fb19fa92aae5b8afa8a1f13dfa8350be5d042558
Kocharigka <onolog@yandex.ru>
2021-10-18 21:31:45 +0500

    fix attack anims

--- Assets/AttackBehaviour.cs
+++ Assets/AttackBehaviour.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveBehaviour : StateMachineBehaviour
+public class AttackBehaviour : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        PlayerController.instance.moveSpeed = 0;
+        PlayerController.instance.canFlip = false;
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -17,10 +18,11 @@ public class MoveBehaviour : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        PlayerController.instance.moveSpeed = PlayerController.instance.defaultMoveSpeed;
+        PlayerController.instance.canFlip = true;
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
--- Assets/Scripts/ControlScripts/PlayerController.cs
+++ Assets/Scripts/ControlScripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector2 direction;
-    private float moveSpeed = 5f;
+    public float moveSpeed;
     public Animator animator;
     private float attackRange = 1f;  
     private int maxHealth=100;
@@ -15,10 +15,14 @@ public class PlayerController : MonoBehaviour
     public Transform projectileHolder;
     public bool isAttacking = false;
     public static PlayerController instance;
+    public float defaultMoveSpeed=5f;
+    public bool canFlip;
 
     private void Awake()
     {
         instance = this;
+        moveSpeed = defaultMoveSpeed;
+        canFlip = true;
     }
     public float AttackRange
     {
@@ -40,19 +44,12 @@ public class PlayerController : MonoBehaviour
     {
         direction.x = Input.GetAxisRaw("Horizontal");
         direction.y = Input.GetAxisRaw("Vertical");
-        if (direction!=new Vector2(0,0))
+        if (direction!=new Vector2(0,0) && canFlip)
         {
             animator.SetFloat("Horizontal", direction.x);
             animator.SetFloat("Vertical", direction.y);
         }
-        if (!isAttacking)
-        {
-            animator.SetFloat("Speed", direction.sqrMagnitude);
-        }
-        else
-        {
-            animator.SetFloat("Speed", 0);
-        }
+        animator.SetFloat("Speed", direction.sqrMagnitude);
         Attack();
 
 
